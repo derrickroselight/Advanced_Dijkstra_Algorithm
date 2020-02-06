9a73b678948f4b818bec025851950306ca98c88a
CHAO-YU CHU <36261467+derrickroselight@users.noreply.github.com>
2020-02-06 17:29:48 +0800

    change to English.ver

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,34 +1,26 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Oct 13 09:32:52 2019
-
-@author: Light
-"""
 import numpy as np
 import pandas as pd
-#讀入資料
 
-df = pd.read_csv('/Users/Light/PythonData/dijkstra2.txt', sep=" ", dtype=object,header=None, names=["start", "end", "dis", "width" ,"bool"],engine='python')
-df['Route'] = df.index
+df = pd.read_csv('/Users/Light/PythonData/dijkstra.txt', sep=" ", dtype=object,header=None, names=["start", "end", "dis", "width" ,"bool"],engine='python')
+df['Road'] = df.index
 
-# 建立距離與寬度矩陣
+# Set up distance and width matrices 
 # inf = infinity
-# d = distance, w = width, r = route
+# d = distance, w = width, r = road
 d = np.zeros((int(df.iloc[0][0]), int(df.iloc[0][0])), dtype=int)
 w = np.zeros((int(df.iloc[0][0]), int(df.iloc[0][0])), dtype=int)
 r = np.zeros((int(df.iloc[0][0]), int(df.iloc[0][0])), dtype=int)
 inf = 99999999
 tsp = {1:'Walk', 2:'Bike', 3:'Scooter', 4:'Car', 5:'Bus'}
 width = {1:0.5, 2:1.5, 3:2, 4:4, 5:6}
-#讀入起點 終點 交通工具
-start, end, method = map(int, input().split(' '))
 
-# n個點 m個邊
+# n : number of nodes 
+# m : number of roads
+# 
 n = int(df.iloc[0][0])
 m = len(df.index) - 1
 
-# 初始化
+# initializion
 for i in range(0, n):
     for j in range(0, n):
         if i == j : 
@@ -50,13 +42,17 @@ for i in range(0, n):
         else:
             r[i][j] = inf               
             
-# 輸入距離資料與路幅資料與路名
+# Input distance, width, and road data into respective matrices
 for i in range(1, m+1):
     d[int(df.iloc[i][0])][int(df.iloc[i][1])] = int(df.iloc[i][2])
     w[int(df.iloc[i][0])][int(df.iloc[i][1])] = int(df.iloc[i][3])
     r[int(df.iloc[i][0])][int(df.iloc[i][1])] = int(df.iloc[i][5])
+  
+# Choose start node, end node, and transportation  
+start, end, method = map(int, input().split(' '))
     
-# 初始距離陣列，從起點到其餘各點的初始路程
+    
+# initial distance array to record the beginning status
 record = [[],[],[],[],[],[],[]]
 dis = np.zeros(int(df.iloc[0][0]), dtype=int)
 for i in range(0, n):
@@ -69,13 +65,13 @@ for i in range(0, n):
 for i in range(0, n):
     print(dis[i])        
 
-# 初始 book 陣列
+# initial "sure" array
 sure = np.zeros(int(df.iloc[0][0]), dtype=int)
 sure[start] = 1
 
 # Dijkstra Algorithm
 for i in range(0, n-1):
-    # 找到離 start 點最近的頂點
+    # Find the nearest node to start node
     min = inf
     for j in range(0, n):
         if (sure[j] == 0) and (dis[j] < min) and (w[start][j] >= width[method]):
@@ -93,7 +89,7 @@ for i in range(0, n-1):
                       record[v].append(record[u][k])
                 record[v].append(r[u][v])
 
-# 輸出
+# Output
 for i in range(0, n):
     print(dis[i])
 
@@ -108,9 +104,3 @@ elif record[end] == []: print('start = end')
 else:
     for i in range(len(record[end])):
         print("%d -> %d Distance : %d Width : %d Limits : %s" %(int(df.iloc[record[end][i]][0]),int(df.iloc[record[end][i]][1]),int(df.iloc[record[end][i]][2]),int(df.iloc[record[end][i]][3]),df.iloc[record[end][i]][4]) )
-
-
-
-
-
-
